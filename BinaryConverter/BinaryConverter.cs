b04f7f63d38a2e9f07f3bc412657e9e2888c3bda
Armin <62218506+SagMeinenNamen@users.noreply.github.com>
2022-01-09 22:26:22 +0100

    Add files via upload

---
+++ BinaryConverter/BinaryConverter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryConverter
+{
+    public class BinaryConverter
+    {
+        public static string ToText(string binary)
+        {
+            var text = GetBytesFromBinaryString(binary);
+            return Encoding.ASCII.GetString(text);
+        }
+
+        public static string ToBinary(string input)
+        {
+            input = reverseString(input);
+            BitArray bitArray = new BitArray(Encoding.UTF8.GetBytes(input));
+            string bits = "";
+            bits = writeBits(bits, bitArray);
+            return reverseString(bits);
+        }
+
+        private static string writeBits(string bits, BitArray bitArray)
+        {
+            foreach (bool bit in bitArray)
+            {
+                if (bit == true)
+                {
+                    bits += "1";
+                }
+                else
+                {
+                    bits += "0";
+                }
+            }
+
+            return bits;
+        }
+
+        private static Byte[] GetBytesFromBinaryString(String binary)
+        {
+            var list = new List<Byte>();
+
+            for (int i = 0; i < binary.Length; i += 8)
+            {
+                String t = binary.Substring(i, 8);
+
+                list.Add(Convert.ToByte(t, 2));
+            }
+
+            return list.ToArray();
+        }
+
+        private static string reverseString(string str)
+        {
+            string reversedString = "";
+            foreach (char c in str)
+            {
+                reversedString = c + reversedString;
+            }
+
+            return reversedString;
+        }
+    }
+}
---
+++ BinaryConverter/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TextCopy;
+
+namespace BinaryConverter
+{
+    internal class Program
+    {
+        static Clipboard cb = new Clipboard();
+        static void Main(string[] args)
+        {
+            Console.Title = "Binary Converter";
+            while (true)
+            {
+                string cbText = cb.GetText();
+                if (cbText != null)
+                {
+                    if (shouldConvertToBinary(cbText) == true)
+                    {
+                        TextToBinary();
+                    }
+                    else
+                    {
+                        ConversionBetweenTextOrBinary(cbText);
+                    }
+                }
+                else
+                {
+                    TextToBinary();
+                }
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+
+        private static void ConversionBetweenTextOrBinary(string cbText)
+        {
+            Console.WriteLine("Binary data detected, should it be converted to text? Write y/yes to confirm: ");
+            string input = Console.ReadLine();
+            if (input == "y" || input == "yes")
+            {
+                Console.WriteLine(BinaryConverter.ToText(cbText));
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("You choosed no, type the input text which should be converted to binary.");
+                Console.WriteLine();
+                TextToBinary();
+            }
+        }
+
+        private static void TextToBinary()
+        {
+            Console.WriteLine("Input: ");
+            string text = Console.ReadLine();
+            Console.WriteLine(BinaryConverter.ToBinary(text));
+        }
+
+
+        private static bool shouldConvertToBinary(string input)
+        {
+            foreach (var c in input)
+            {
+                if ((c.ToString() != "0") && (c.ToString() != "1"))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+     
+
+      
+    }
+}
